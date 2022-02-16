35ac78c3ebeb8fd7789f045141b9073059647286
estesthebestes <esteshunter22@gmail.com>
2022-02-16 15:27:29 -0600

    learned how to use bool

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //a variable is a box in which we can store a value 
- let js = 'amazing';
+ /* let js = 'amazing';
  console.log(40 +8 +23 -10);
 
  console.log('Jonas'); 
@@ -18,8 +18,39 @@ let continent = 'North America';
 console.log(continent)
 
 let population = '300 million people'; 
-console.log(population);
+console.log(population); */
 
-//remember that javascript has dynamic typing 
+//remember that javascript has dynamic typing!!!!!!
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 2343);
+// console.log(typeof 'Hunter');
+
+
+// javascriptIsFun = 'Yes!'; 
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1997;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+// let isIsland = false;
+// console.log(isIsland);
+
+// let language;
+// console.log(language);
+
+// console.log(typeof population);
+
+// console.log(typeof country); 
 
 
