9d8282b0adbff7e324cc570ef1d0762d848553d3
estesthebestes <esteshunter22@gmail.com>
2022-02-16 15:11:13 -0600

    new project to practice javascript

---
+++ script.js
@@ -0,0 +1,25 @@
+//a variable is a box in which we can store a value 
+ let js = 'amazing';
+ console.log(40 +8 +23 -10);
+
+ console.log('Jonas'); 
+ console.log("Hunter");
+ console.log('23');
+
+ let firstName = "Hunter";
+
+ console.log(firstName);
+
+
+let country = 'United States of America';
+console.log(country);
+
+let continent = 'North America';
+console.log(continent)
+
+let population = '300 million people'; 
+console.log(population);
+
+//remember that javascript has dynamic typing 
+
+
